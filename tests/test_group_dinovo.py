807d62515a0225b77dc67e24f4294ed8391cc489
aigulmustafina <aimustafina2016@gmail.com>
2023-04-27 16:55:07 +0600

    Revert "Test ty (#138)"
    This reverts commit 1d4935c9b7dcb1b44bd0a48777ea0b81154fcdd7.

--- tests/test_group_dinovo.py
+++
@@ -1,8 +0,0 @@
-def test_should_open_given_link(driver):
-    driver.get('https://openweathermap.org/')
-    assert 'openweathermap' in driver.current_url
-
-
-def test_check_page_title(driver):
-    driver.get('https://openweathermap.org/')
-    assert driver.title == 'Сurrent weather and forecast - OpenWeatherMap'
