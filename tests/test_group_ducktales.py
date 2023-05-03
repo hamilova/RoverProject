3329e19e7db9839f39d54a9d7f3aea936b5a5855
Elena_S <sosnitsky.e@gmail.com>
2023-05-03 22:18:20 +0300

    test_tc_001_01_01_verify_city_name_displayed_by_zip add (#249)

---
+++ tests/test_group_ducktales.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def test_tc_001_01_01_verify_city_name_displayed_by_zip(driver):
+    driver.get('https://openweathermap.org/')
+    WebDriverWait(driver, 10).until_not(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, 'div.owm-loader-container > div')))
+    search_city_field = driver.find_element(By.CSS_SELECTOR, "input[placeholder='Search city']")
+    search_city_field.send_keys('66002')
+    search_button = driver.find_element(By.CSS_SELECTOR, "button[class ='button-round dark']")
+    search_button.click()
+    search_option = WebDriverWait(driver, 15).until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, 'ul.search-dropdown-menu li:nth-child(1) span:nth-child(1)')))
+    search_option.click()
+    expected_city = 'Atchison, US'
+    WebDriverWait(driver, 20).until(EC.text_to_be_present_in_element(
+        (By.CSS_SELECTOR, '.grid-container.grid-4-5 h2'), 'Atchison'))
+    displayed_city = driver.find_element(By.CSS_SELECTOR, '.grid-container.grid-4-5 h2').text
+    assert displayed_city == expected_city
