d7a962cb1950811ea45581df6fb32b0adb4d6e74
AndreyBozhko <124488708+AndrSam1@users.noreply.github.com>
2023-05-03 21:01:18 +0300

    TC_001.02.02 | Main page > Search city widget > Switching temperature… (#246)
    TC_001.02.02 | Main page > Search city widget > Switching temperature measuring system > Verify temperature switched on imperial system

--- tests/test_group_trust_me_i_am_engineer.py
+++ tests/test_group_trust_me_i_am_engineer.py
@@ -4,11 +4,15 @@ from selenium.webdriver.support import expected_conditions as EC
 URL = 'https://openweathermap.org/'
 load_div = (By.CSS_SELECTOR, 'div.owm-loader-container > div')
 metric_button_loc = (By.XPATH, "//div[@class='switch-container']/div[contains(text(), 'Metric')]")
+imperial_button_loc = (By.XPATH, "//div[@class='switch-container']/div[contains(text(), 'Imperial')]")
 current_temp_loc = (By.CSS_SELECTOR, "div.current-temp span.heading")
 
-
-def test_TC_001_02_01_verify_temperature_switched_on_metric_system(driver, open_and_load_main_page, wait):
+def test_TC_001_02_01_verify_temperature_switched_on_metric_system(driver, open_and_load_main_page):
     driver.find_element(*metric_button_loc).click()
-    wait.until_not(EC.presence_of_element_located(load_div))
     current_temp = driver.find_element(*current_temp_loc)
-    assert "°C" in current_temp.text, "The current temperature does not correspond to the metric"
+    assert "°C" in current_temp.text, "The current temperature does not correspond to the metric system"
+
+def test_TC_001_02_02_verify_temperature_switched_on_imperial_system(driver, open_and_load_main_page):
+    driver.find_element(*imperial_button_loc).click()
+    current_temp = driver.find_element(*current_temp_loc)
+    assert "°F" in current_temp.text, "The current temperature does not correspond to the imperial system"
