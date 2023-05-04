00ceb503652b94ab659b6d10060e0eaf1ecfe2ba
Aleksandra Kozhevnikova <aleksandra.kozhe@gmail.com>
2023-05-04 08:38:18 +0300

    removed fixtures, lines 7 and 9

--- tests/test_group_qa_ua.py
+++ tests/test_group_qa_ua.py
@@ -4,21 +4,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 URL = 'https://openweathermap.org/'
-load_div = (By.CSS_SELECTOR, 'div.owm-loader-container > div')
 marketplace_link = (By.CSS_SELECTOR, '#desktop-menu ul li:nth-child(4) a')
-marketplace = 'https://home.openweathermap.org/marketplace'
-
-
-@pytest.fixture()
-def wait(driver):
-    wait = WebDriverWait(driver, 25)
-    yield wait
-
-
-@pytest.fixture()
-def open_and_load_page(driver, wait):
-    driver.get(URL)
-    wait.until_not(EC.presence_of_element_located(load_div))
 
 
 def test_TC_002_03_07_verify_marketplace_link_redirects_to_valid_page(driver, open_and_load_main_page, wait):
@@ -26,4 +12,4 @@ def test_TC_002_03_07_verify_marketplace_link_redirects_to_valid_page(driver, op
     driver.find_element(*marketplace_link).click()
     handles = driver.window_handles
     driver.switch_to.window(handles[1])
-    assert driver.current_url == marketplace
+    assert driver.current_url == 'https://home.openweathermap.org/marketplace'
