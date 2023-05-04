5b7c9fae030ff5ff6635a23bffc73ff25aa01508
Anastasiya Pronina <nastyapro876@gmail.com>
2023-05-04 17:24:14 +0300

    TC_002.03.08 | Header > Links > Navigation > "Pricing" link leads to … (#247)
    * TC_002.03.08 | Header > Links > Navigation > "Pricing" link leads to the correct page
    
    * TC_002.03.08 | Header > Links > Navigation > "Pricing" link leads to the correct page (code refactoring)
    
    * TC_002.03.08 | Header > Links > Navigation > "Pricing" link leads to the correct page (code refactoring)
    
    ---------
    
    Co-authored-by: Anastasiya Pronina <apronina@piesoft.us>

---
+++ tests/test_pandoras_box.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+
+URL = 'https://openweathermap.org/'
+BUTTON_PRICING = (By.XPATH, '//div[@id="desktop-menu"]//a[text()="Pricing"]')
+DISPLAYED_TITLE = (By.CSS_SELECTOR, 'h1.breadcrumb-title')
+
+def test_TC_002_03_08_open_pricing(driver):
+    driver.get(URL)
+    button_pricing = driver.find_element(*BUTTON_PRICING)
+    action_chains = ActionChains(driver)
+    action_chains.move_to_element(button_pricing)
+    driver.execute_script("arguments[0].click();", button_pricing)
+    expected_title = "Pricing"
+    displayed_title = driver.find_element(*DISPLAYED_TITLE).text
+    assert displayed_title == expected_title
