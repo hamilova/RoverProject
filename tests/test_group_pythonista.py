b426ff3fc773d76bfc7dc14e7b3e9e07ab4b658e
Tanya Butakova <tetiana1007@outlook.com>
2023-05-04 16:07:05 -0400

    TC_002.03.05 | Header > Links > Navigation > Dashboard link is clickable and visible

--- tests/test_group_pythonista.py
+++ tests/test_group_pythonista.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 FOOTER_PANEL = (By.XPATH, '//*[@id="stick-footer-panel"]/div')
 BTN_ALLOW_ALL = (By.CLASS_NAME, "stick-footer-panel__link")
 FOOTER_COPYRIGHT = (By.XPATH, "//div[@class='horizontal-section my-5']/div[1]")
-
+DASHBOARD_LINK = (By.XPATH, '//div[@id="desktop-menu"]//a[contains(@href, "/weather-dashboard")]')
 
 def test_TC_003_11_01_verify_the_copyright_information_is_present_on_the_page(driver, open_and_load_main_page, wait):
     wait.until(EC.element_to_be_clickable(FOOTER_PANEL))
@@ -19,3 +19,8 @@ def test_TC_003_11_01_verify_the_copyright_information_is_present_on_the_page(dr
         "The footer is not displayed or does not contain the expected text"
 
 
+def test_TC_002_03_05_dashboard_is_visible_and_clickable(driver, open_and_load_main_page,wait):
+    wait.until(EC.element_to_be_clickable(DASHBOARD_LINK))
+    dashboard_tab = driver.find_element(*DASHBOARD_LINK)
+    expected_dashboard_label = 'Dashboard'
+    assert dashboard_tab.is_displayed() and dashboard_tab.is_enabled() and expected_dashboard_label in dashboard_tab.text
