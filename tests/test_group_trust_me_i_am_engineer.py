0961530de3cdc274f3117cd739639a18de986c49
AndreyBozhko <124488708+AndrSam1@users.noreply.github.com>
2023-05-05 15:18:27 +0300

    TC_001.02.03 | Main page > Search city widget > Switching temperature measuring system > Verify switching temperature metric system button displayed and clickable (#268)

--- tests/test_group_trust_me_i_am_engineer.py
+++ tests/test_group_trust_me_i_am_engineer.py
@@ -2,7 +2,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 URL = 'https://openweathermap.org/'
-load_div = (By.CSS_SELECTOR, 'div.owm-loader-container > div')
 metric_button_loc = (By.XPATH, "//div[@class='switch-container']/div[contains(text(), 'Metric')]")
 imperial_button_loc = (By.XPATH, "//div[@class='switch-container']/div[contains(text(), 'Imperial')]")
 current_temp_loc = (By.CSS_SELECTOR, "div.current-temp span.heading")
@@ -16,3 +15,8 @@ def test_TC_001_02_02_verify_temperature_switched_on_imperial_system(driver, ope
     driver.find_element(*imperial_button_loc).click()
     current_temp = driver.find_element(*current_temp_loc)
     assert "°F" in current_temp.text, "The current temperature does not correspond to the imperial system"
+
+def test_TC_001_02_03_verify_temperature_metric_button_displayed_clickable(driver, open_and_load_main_page, wait):
+    metric_button = wait.until(EC.element_to_be_clickable(metric_button_loc))
+    assert metric_button.is_displayed() and metric_button.is_enabled(), \
+        "The temperature switch button in the metric system is not displayed or is not clickable"
