aa638dda522792f8e6b48eddbaff49a79f2ef768
Andrey Zhandarov <117146794+ChosenOnelru@users.noreply.github.com>
2023-05-05 12:19:41 +0300

    TC_002.01.03 > Header > Logo > Verify Logo is visible from all pages specified in data (#250)
    * TC_002.01.03
    Header > Logo > Verify Logo is visible from all pages specified in data

--- tests/test_pandoras_box.py
+++ tests/test_pandoras_box.py
@@ -1,9 +1,23 @@
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+import pytest
 
 URL = 'https://openweathermap.org/'
 BUTTON_PRICING = (By.XPATH, '//div[@id="desktop-menu"]//a[text()="Pricing"]')
 DISPLAYED_TITLE = (By.CSS_SELECTOR, 'h1.breadcrumb-title')
+
+logo_locator = (By.XPATH, '//*[@class="logo"]/a/img')
+URLs = ['https://openweathermap.org/',
+        'https://openweathermap.org/guide',
+        'https://openweathermap.org/api',
+        'https://openweathermap.org/weather-dashboard',
+        'https://openweathermap.org/price',
+        'https://openweathermap.org/our-initiatives',
+        'https://openweathermap.org/examples',
+        'https://home.openweathermap.org/users/sign_in',
+        'https://openweathermap.org/faq',
+        'https://openweathermap.org/appid',
+        'https://home.openweathermap.org/questions']
 
 def test_TC_002_03_08_open_pricing(driver):
     driver.get(URL)
@@ -14,3 +28,9 @@ def test_TC_002_03_08_open_pricing(driver):
     expected_title = "Pricing"
     displayed_title = driver.find_element(*DISPLAYED_TITLE).text
     assert displayed_title == expected_title
+
+@pytest.mark.parametrize('URL', URLs)
+def test_TC_002_01_03_Logo_is_visible(driver, wait, URL):
+    driver.get(URL)
+    logo = driver.find_element(*logo_locator)
+    assert logo.is_displayed(), "Logo is not visible"
