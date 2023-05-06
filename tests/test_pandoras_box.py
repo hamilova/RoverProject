4d1e9e89e100847d74e0abb85a23065186b9cc66
blinkova-mv <129224688+blinkova-mv@users.noreply.github.com>
2023-05-06 17:25:37 +0300

    TC_002.02.03 | Header > Search field > Search city > Verify notificat… (#256)
    * TC_002.02.03 | Header > Search field > Search city > Verify notification for invalid city name
    
    * TC_002.02.03 | Header > Search field > Search city > Verify notification for invalid city name

--- tests/test_pandoras_box.py
+++ tests/test_pandoras_box.py
@@ -4,12 +4,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import pytest
 
+
 URL = 'https://openweathermap.org/'
 BUTTON_PRICING = (By.XPATH, '//div[@id="desktop-menu"]//a[text()="Pricing"]')
 DISPLAYED_TITLE = (By.CSS_SELECTOR, 'h1.breadcrumb-title')
 FIELD_WEATHER_IN_YUOR_CITY = (By.CSS_SELECTOR, "#desktop-menu input[placeholder='Weather in your city']")
 ALERT_NOTIFICATION = (By.CSS_SELECTOR, "#forecast_list_ul .alert.alert-warning")
 STRING_ENTERED_CITY = (By.CSS_SELECTOR, "#search_str")
+
 
 logo_locator = (By.XPATH, '//*[@class="logo"]/a/img')
 URLs = ['https://openweathermap.org/',
@@ -24,6 +26,7 @@ URLs = ['https://openweathermap.org/',
         'https://openweathermap.org/appid',
         'https://home.openweathermap.org/questions']
 
+
 def test_TC_002_03_08_open_pricing(driver):
     driver.get(URL)
     button_pricing = driver.find_element(*BUTTON_PRICING)
@@ -33,6 +36,18 @@ def test_TC_002_03_08_open_pricing(driver):
     expected_title = "Pricing"
     displayed_title = driver.find_element(*DISPLAYED_TITLE).text
     assert displayed_title == expected_title
+
+
+def test_TC_002_02_03_verify_result_of_search_for_invalid_city_name(driver, open_and_load_main_page, wait):
+    search_weather_in_your_city = driver.find_element(*FIELD_WEATHER_IN_YUOR_CITY)
+    entered_invalid_city_name = "LJKJJ"
+    search_weather_in_your_city.send_keys(entered_invalid_city_name)
+    actions = ActionChains(driver)
+    actions.send_keys(Keys.ENTER).perform()
+    wait.until(EC.presence_of_element_located(ALERT_NOTIFICATION))
+    displayed_notification = driver.find_element(*ALERT_NOTIFICATION)
+    notification = displayed_notification.text
+    assert notification == "×\nNot found"
 
 
 def test_TC_002_02_04_verify_displaying_entered_city_name_in_Search_field(driver, open_and_load_main_page, wait):
@@ -51,3 +66,4 @@ def test_TC_002_01_03_Logo_is_visible(driver, wait, URL):
     driver.get(URL)
     logo = driver.find_element(*logo_locator)
     assert logo.is_displayed(), "Logo is not visible"
+
