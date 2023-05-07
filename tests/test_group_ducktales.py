d5e12b39b428a49a54ea7493dc6bb205e94d52c7
Elena_S <sosnitsky.e@gmail.com>
2023-05-07 16:21:27 +0300

    TC_003.09.01 | Footer > Download OpenWeather app module > Visibility, structure, links сlickability> The module title's display (#289)

--- tests/test_group_ducktales.py
+++ tests/test_group_ducktales.py
@@ -11,6 +11,7 @@ BTN_SEARCH = By.CSS_SELECTOR, "button[class ='button-round dark']"
 SEARCH_DROPDOWN_MENU = By.CLASS_NAME, 'search-dropdown-menu'
 SEARCH_DROPDOWN_MENU_FIRST_CHILD = By.CSS_SELECTOR, 'ul.search-dropdown-menu li:nth-child(1) span:nth-child(1)'
 SEARCH_DROPDOWN_MENU_FIRST_CHILD_TEXT = By.CSS_SELECTOR, '.grid-container.grid-4-5 h2'
+MODULE_DOWNLOAD_OPENWEATHER_APP = By.XPATH, "//div[@class='my-5']/p"
 
 
 def test_tc_001_01_01_verify_city_name_displayed_by_zip(driver, open_and_load_main_page, wait):
@@ -45,4 +46,12 @@ def test_tc_001_02_04_01_switch_toggle_buttons(driver, open_and_load_main_page, 
     # Verify that toggle buttons are displayed and clickable
     assert metric_button.is_displayed() and imperial_button.is_displayed()
     assert metric_button.is_enabled() and imperial_button.is_enabled()
+
+
+def test_tc_003_09_01_the_module_title_display(driver, open_and_load_main_page, wait):
+    expected_module_title = "Download OpenWeather app"
+    module_download_openweather_app = driver.find_element(*MODULE_DOWNLOAD_OPENWEATHER_APP)
+    module_download_openweather_app.location_once_scrolled_into_view
+    actual_module_title = module_download_openweather_app.text
+    assert actual_module_title == expected_module_title
 
