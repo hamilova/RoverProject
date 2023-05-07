8862dd7dd843191e85b68e23f354e576624f91b9
Svetlana Markelova <127980916+SvetlanaIM@users.noreply.github.com>
2023-05-07 20:50:43 +0300

    AT_003_10_03 | Footer > Social media module > Icons-links visibility and сlickability > Verify the visibility of GitHub icon-link (#287)

--- tests/test_group_roma.py
+++ tests/test_group_roma.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 footer_website_locator = (By.ID, "footer-website")
 company_title_locator = (By.XPATH, "//p[@class='section-heading' and text()='Company']")
 company_content_locator = (By.CSS_SELECTOR, ".footer-section > div > p")
-
+gitHub_icon_image = (By.XPATH, "//div[@class='social']//a[6]/img")
 
 
 def test_TC_003_07_01_visibility_of_the_company_module(driver, open_and_load_main_page, wait):
@@ -13,3 +13,10 @@ def test_TC_003_07_01_visibility_of_the_company_module(driver, open_and_load_mai
     assert company_title.is_displayed()
     content = driver.find_element(*company_content_locator)
     assert content.is_displayed()
+
+
+def test_TC_003_10_03_visibility_of_GitHub_icon(driver, open_and_load_main_page, wait):
+    footer_website = driver.find_element(*footer_website_locator)
+    driver.execute_script("arguments[0].scrollIntoView();", footer_website)
+    github_icon = driver.find_element(*gitHub_icon_image)
+    assert github_icon.is_displayed()
