4a537fce9fca0616aa924ffae2c54c71e660b267
Okun91 <123413129+Okun91@users.noreply.github.com>
2023-05-07 08:14:38 +0300

    TC_003.07.01 | Footer > Company module > Visibility, content | Visibi… (#270)
    * TC_003.07.01 | Footer > Company module > Visibility, content | Visibility of the Company module

---
+++ tests/test_group_roma.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+
+footer_website_locator = (By.ID, "footer-website")
+company_title_locator = (By.XPATH, "//p[@class='section-heading' and text()='Company']")
+company_content_locator = (By.CSS_SELECTOR, ".footer-section > div > p")
+
+
+
+def test_TC_003_07_01_visibility_of_the_company_module(driver, open_and_load_main_page, wait):
+    footer_website = driver.find_element(*footer_website_locator)
+    driver.execute_script("arguments[0].scrollIntoView();", footer_website)
+    company_title = driver.find_element(*company_title_locator)
+    assert company_title.is_displayed()
+    content = driver.find_element(*company_content_locator)
+    assert content.is_displayed()
